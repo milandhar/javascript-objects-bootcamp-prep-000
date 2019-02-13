57006588f847be64638ba9db5f33456a2f0c611a
Milan Dhar <milan.dhar@gmail.com>
2019-02-13 16:37:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete [artistName];
+  delete playlist[artistName];
   return playlist;
 }
